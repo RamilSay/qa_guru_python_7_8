75321917ab2888e7f89ba5a5fc68dd4d2cb33a54
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-29 21:49:17 +0700

    working

--- homework/models.py
+++ homework/models.py
@@ -58,7 +58,7 @@ class Cart:
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
         """
-        self.products[product] = self.products.get(product,0) + buy_count
+        self.products[product] = self.products.get(product, 0) + buy_count
 
 
     def remove_product(self, product: Product, remove_count=None):
